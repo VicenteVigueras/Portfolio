e855e878b99ff968d031866c8cb882e539d76e1b
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2025-03-17 23:24:57 -0400

    feat: add profile pic and description

--- my-portfolio/src/app/components/homepage.tsx
+++ my-portfolio/src/app/components/homepage.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
+
 export function HomePage() {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="text-3xl font-bold border border-indigo-600">Hello World</div>
+        <div className="flex items-start justify-center h-screen flex-col ml-30">
+            <div className="space-y-4 text-center">
+                <div className="">
+                       <Image 
+                        src="/profile.jpg"
+                        width={250}
+                        height={250}
+                        alt="Profile Picture"
+                         className="rounded-3xl"
+                        />
+                    </div>
+                    <div className="text-md font-bold ">
+                        Description
+                    </div>
+                </div>
         </div>
     );
 };
